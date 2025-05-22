adca9a9a81b04260dd0941458f996583df80086b
tensey3 <kikuchitensei428@gmail.com>
2025-05-22 11:22:05 +0900

    a

---
+++ jugyou/chapter2/Code0208.py
@@ -0,0 +1,5 @@
+members = ["工藤","松田","浅木",]
+members.append("菅原")
+members.append("湊")
+members.append("朝香")
+print(members)
--- jugyou/chapter2/Code206.py
+++ jugyou/chapter2/Code206.py
@@ -1,3 +1,6 @@
 score = [88, 90 ,95]
 total = sum(score)
 print(f"合計{total}点")
+
+a =["くどう":"あさが", "まつだ:ぬま"]
+print(a)
