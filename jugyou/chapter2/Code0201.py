7948c0e82dcac4ad13af4403c6fa5f838e8c18e6
tensey3 <kikuchitensei428@gmail.com>
2025-05-22 10:31:42 +0900

    a

--- jugyou/chapter2/Code0201.py
+++ jugyou/chapter2/Code0201.py
@@ -5,3 +5,7 @@ total = netwoek + database + security
 avg = total / 3
 print(f"合計点:{total}")
 print(f"平均点:{avg}")
+
+a = [netwoek, database, security]
+print(f"合計点:{sum(a)}")
+print(f"平均点:{sum(a)/len(a)}")
